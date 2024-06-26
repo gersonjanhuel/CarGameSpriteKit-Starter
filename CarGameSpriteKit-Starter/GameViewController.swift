e95464d4f941933f26caf445baebd883dad25dbf
Gerson Janhuel <gersonjanhuel@gmail.com>
2024-06-26 14:32:20 +0700

    clean project

--- CarGameSpriteKit-Starter/GameViewController.swift
+++ CarGameSpriteKit-Starter/GameViewController.swift
@@ -17,15 +17,4 @@ class GameViewController: UIViewController {
         
     }
 
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
 }
